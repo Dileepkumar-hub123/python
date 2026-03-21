02cd2bc07327abd9409ed7c290de6b4a11e2fe19
Dileepkumar-hub123 <jdileepkumar1@gmail.com>
2026-03-21 22:13:33 +0530

    Add files via upload

---
+++ #palindrom or not.py
@@ -0,0 +1,13 @@
+#palindrom or not
+n=int(input("enter :"))
+rev=0
+temp=n
+while n>0 :
+ rem=n%10
+ rev = rev *10 + rem
+ n=n//10
+if temp == rev :
+ print("palindrom")
+else :
+ print("not palindrom") 
+ 
