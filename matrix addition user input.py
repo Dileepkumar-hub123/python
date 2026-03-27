e23f76fd6a71a47f56ab1449559a03ea59cb0a3e
J Dileep Kumar <jdileepkumar1@gmail.com>
2026-03-27 22:31:42 +0530

    Add files via upload

---
+++ matrix addition user input.py
@@ -0,0 +1,19 @@
+row=int(input("enter  row :"))
+col=int(input("enter  col :"))
+a=[]
+b=[]
+row=[]
+result=[]
+for i in range(row):
+   row=[]
+   for j in range(col) :
+      row.append(int(input()))
+   a.append(row)
+
+for i in range(row):
+   row=[]
+   for j in range(col) :
+      row.append(int(input()))
+   b.append(row)
+
+for i in range(row):
