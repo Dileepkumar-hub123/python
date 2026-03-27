c872263968d5ac4d5a820cd1043cf252e9e432db
J Dileep Kumar <jdileepkumar1@gmail.com>
2026-03-27 22:32:02 +0530

    Add files via upload

---
+++ matrix addition.py
@@ -0,0 +1,13 @@
+A = [[1, 2], [3, 4]]
+B = [[5, 6], [9, 8]]
+
+result = []
+
+for i in range(len(A)):
+    row = []
+    for j in range(len(A[0])):
+        row.append(A[i][j] + B[i][j])
+    result.append(row)
+
+for r in result:
+    print(r)
