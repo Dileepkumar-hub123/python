09cc28c07a69c499f28d56a5f7d8ea49dd305bd3
J Dileep Kumar <jdileepkumar1@gmail.com>
2026-04-21 21:40:47 +0530

    Add files via upload

---
+++ string functions.py
@@ -0,0 +1,15 @@
+a='dileep'
+b='KUMAR'
+c='cse3'
+d='i like java'
+print(a.islower())
+print(b.isupper())
+print(a.isdigit())
+print(a.isalpha())
+print(c.isalnum())
+print(c.upper())
+print(b.lower())
+print(len(a))
+print(d.replace('java','python'))
+print(a.title())
+print(a.capitalize())
